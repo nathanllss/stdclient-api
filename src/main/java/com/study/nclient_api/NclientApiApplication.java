632773824206e65deec907696c4442345fd4cde9
Nathan <nathan.llucasl@gmail.com>
2024-12-30 16:01:24 -0300

    initializing repo

---
+++ src/main/java/com/study/nclient_api/NclientApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.nclient_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NclientApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NclientApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/nclient_api/NclientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.nclient_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NclientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
